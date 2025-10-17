eaa4775458f50e1feea7f85db5d5b2f497dc609e
Alejandro4587 <200733732+Alejandro4587@users.noreply.github.com>
2025-10-17 10:15:28 +0200

    widget column

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,8 +6,18 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlueAccent,
-      body: Center(child: const Text('HomeScreen')),
+      backgroundColor: const Color.fromARGB(255, 207, 176, 189),
+      appBar: AppBar(title: Text('HomeScreen'), elevation: 10.0),
+      body: Center(
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Hola mundo este es el home 1"),
+            SizedBox(height: 100, width: 100),
+            Text("Hola mundo este es el home 2"),
+          ],
+        ),
+      ),
     ); // Center
   }
 }
